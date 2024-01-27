e3b0ab89e57bf5241af120268e40124d72dc06c8
Mina Adly <mina2dly@gmail.com>
2024-01-27 18:32:58 +0200

    refactor remote data source

---
+++ lib/core/network/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const baseUrl = "https://api.themoviedb.org/3";
+  static const apiKey = "76e8e23dc591af13d2a4e329c309abed";
+
+  static const nowPlayingMoviesPath =
+      "$baseUrl/movie/now_playing?api_key=$apiKey";
+}
--- lib/movies/data/data_source/movie_remote_data_source.dart
+++ lib/movies/data/data_source/movie_remote_data_source.dart
@@ -1,11 +1,16 @@
 import 'package:dio/dio.dart';
 import 'package:movies_app/core/error/exceptions.dart';
+import 'package:movies_app/core/network/api_constants.dart';
 import 'package:movies_app/movies/data/models/movie_model.dart';
 
-class MovieRemoteDataSource {
+abstract class BaseMovieRemoteDataSource {
+  Future<List<MovieModel>> getNowPlayingMovies();
+}
+
+class MovieRemoteDataSource implements BaseMovieRemoteDataSource {
+  @override
   Future<List<MovieModel>> getNowPlayingMovies() async {
-    final response =
-        await Dio().get("https://api.themoviedb.org/3/movie/now_playing");
+    final response = await Dio().get(ApiConstants.nowPlayingMoviesPath);
 
     if (response.statusCode == 200) {
       return List<MovieModel>.from((response.data["results"] as List).map(
